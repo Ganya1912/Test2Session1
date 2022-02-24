ba0b8c5c776d4e66fc6dac40498d1c49c41f3157
Ganya <97089502+Ganya1912@users.noreply.github.com>
2022-02-24 09:46:09 +0000

    completed

---
+++ p3original.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int input_number()
+{
+  int a;
+  printf("enter a number\n");
+  scanf("%d",&a);
+  return a;
+}
+int is_prime(int n)
+{
+  for(int i=2;i<sqrt(n);i++)
+    {
+      if(n%i )
+    }
+}
